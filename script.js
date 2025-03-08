4858e4dcc225d3a3453ef98ba6aafb2961bb4101
DarrylMathias <mathiasndarryl7@gmail.com>
2025-03-08 16:44:40 +0530

    Added Harmburger Menu Functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+function hamburger(){
+    const innerNav = document.querySelector(".innernav");
+    innerNav.classList.toggle("flex-direction-col")
+    const navElements = document.querySelectorAll(".nav-child");
+    for(i=0; i<navElements.length; i++){
+        navElements[i].classList.toggle("visibility")
+        navElements[i].classList.toggle("expand-top")
+    }
+    document.querySelector(".nav").classList.toggle("flex-direction-col")
+    document.querySelector(".hamburger-logo").classList.toggle("rotate")
+
+}
